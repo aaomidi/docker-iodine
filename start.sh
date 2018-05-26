b5c5a7b8c26acfb3062b20df2b7c9260269d6474
Amir Ali Omidi <amir@aaomidi.com>
2018-05-26 11:36:14 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,4 +10,4 @@ iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -o eth0 -j MASQUERADE
 iptables -S
 iptables -S -t nat
 
-iodined -c -f $TUNNEL_IP $IODINE_HOST -P $IODINE_PASSWORD -n $EXTERNAL_IP
+./bin/iodined -c -f $TUNNEL_IP $IODINE_HOST -P $IODINE_PASSWORD -n $EXTERNAL_IP
